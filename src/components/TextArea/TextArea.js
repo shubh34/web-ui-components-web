da4f09da6c342e1f686a7cf89cafadbd6fd8778c
Shubham Jain <shubhamjain.qwerty@gmail.com>
2020-05-17 19:27:56 +0100

    Text Area

--- src/components/TextArea/TextArea.js
+++ src/components/TextArea/TextArea.js
@@ -3,23 +3,30 @@ import './TextArea.scss';
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
 
-const TextArea = ({ onChange, value = '', className }) => {
-	const css = classNames(
-		'c-text-area',
-		className
-	);
+const TextArea = ({ onChange, value = '', className, maxChar=500, placeholder }) => {
 	const [charCount, setCharacterCount] = useState(value.length);
+	const [isCharacterCountExceeded, setCharacterCountExceed] = useState(value.length > maxChar);
 	const handleTextAreaChange = e => {
 		setCharacterCount(e.target.value.length);
+		setCharacterCountExceed(e.target.value.length > maxChar)
 		onChange(e);
 	};
+	const css = classNames(
+		'c-text-area',
+		{'c-text-area--error': isCharacterCountExceeded},
+		className
+	);
+	const charCountCss = classNames(
+		'c-text-area_char_count',
+		{'c-text-area--error': isCharacterCountExceeded},
+	);
 	return (
 		<>
-			<textarea className={css} onChange={handleTextAreaChange} value={value} />
-			<p className="c-text-area_char_count">
+			<textarea className={css} onChange={handleTextAreaChange} value={value} placeholder={placeholder} />
+			<span className={charCountCss}>
 				<span>{charCount}</span>
-				<span>/500 char.</span>
-			</p>
+				<span>/{maxChar} char.</span>
+			</span>
 		</>
 	);
 };
@@ -27,7 +34,9 @@ const TextArea = ({ onChange, value = '', className }) => {
 TextArea.propTypes = {
 	value: PropTypes.string,
 	className: PropTypes.string,
-	onChange: PropTypes.func
+	onChange: PropTypes.func,
+	maxChar: PropTypes.number,
+	placeholder: PropTypes.string,
 };
 
 export default TextArea;
